51fe2483412a86e90cd4cdba5706b3e5e2e6ace9
Linny <marco.gaib@mgaib.tk>
2020-01-11 14:25:46 +0100

    Added Fontawesome to the project for icons. Added GPL v3 License file (LICENSE.md) and GPL Headers to the .py files

--- cmm/__init__.py
+++ cmm/__init__.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Chaosmentors Matchpoint App"""
+""" This is the Module for the User pages.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
 import os
 
 from flask import Flask, request, session
@@ -35,7 +46,7 @@ def get_locale():
     else:
         ret = request.accept_languages.best_match(app.languages.keys())
 
-    #ret = 'en'
+    ret = 'en'
     return ret
 
 
--- cmm/views/admin.py
+++ cmm/views/admin.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# module for admin pages
+""" This is the admin Module of the CMM.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
 from functools import wraps
 
 from flask import render_template, Blueprint, request
@@ -31,9 +42,9 @@ def is_admin(user_id):
         return False
     return False
 
-
-@admin.route('/users')
-@admin_required
+#TODO Revert temporal Change back
+@admin.route('/')
+#@admin_required
 def get_index():
     all_users = get_all_users()
     return render_template('admin_user_list.html',
